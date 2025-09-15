2094dfd6a8f46692f3a0061c1cc158e520fc894a
Mahmoud Omiesh <mahmoudomiesh2005@gmail.com>
2025-09-15 23:42:12 +0300

    refactor: split getLines logic into another function that uses go routine

--- main.go
+++ main.go
@@ -2,10 +2,54 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	linesCh := make(chan string)
+	line := ""
+
+	go func() {
+		defer close(linesCh)
+		defer f.Close()
+
+		for {
+			buf := make([]byte, 8)
+			n, err := f.Read(buf)
+
+			if err != nil {
+				fmt.Println("it's fucked")
+				break
+			}
+
+			current := string(buf[:n])
+			currentSplit := strings.Split(current, "\n")
+
+			line += currentSplit[0]
+
+			if len(currentSplit) == 1 {
+				continue
+			}
+
+			linesCh <- line
+
+			for i := 1; i < len(currentSplit)-1; i++ {
+				linesCh <- currentSplit[i]
+			}
+
+			line = currentSplit[len(currentSplit)-1]
+		}
+
+		if line != "" {
+			linesCh <- line
+		}
+	}()
+
+	return linesCh
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
@@ -14,36 +58,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	line := ""
+	linesCh := getLinesChannel(file)
 
-	for {
-		buf := make([]byte, 8)
-		n, err := file.Read(buf)
-
-		if err != nil {
-			break
-		}
-
-		current := string(buf[:n])
-		currentSplit := strings.Split(current, "\n")
-
-		line += currentSplit[0]
-
-		if len(currentSplit) == 1 {
-			continue
-		}
-
-		fmt.Printf("read: %s\n", line)
-
-		for i := 1; i < len(currentSplit)-1; i++ {
-			fmt.Printf("read: %s\n", currentSplit[i])
-		}
-
-		line = currentSplit[len(currentSplit)-1]
-	}
-
-	if line != "" {
+	for line := range linesCh {
 		fmt.Printf("read: %s\n", line)
 	}
-
 }
